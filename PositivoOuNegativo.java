95ebf71a8e2dbd92dac1fb04649d7ca1ef1aa48c
gustavoliveira <gustavocoliveira16@gmail.com>
2026-03-12 19:47:20 -0300

    Create PositivoOuNegativo.java

---
+++ PositivoOuNegativo.java
@@ -0,0 +1,23 @@
+import java.util.scanner;
+public class ParOuImpar {
+  public static void main(strings[]args ) {
+  scanner sc = new scanner(system.in);
+
+  Float num;
+
+  system.out.println("Digite o numero: ");
+  num = sc.nextFloat();
+
+  if(numero > 0){
+    system.out.print("O numero é par");
+  }
+  else if(numero < 0){
+    system.out.print("o numero é impar");
+  }
+  else {
+    system.out.print("O numero é zero!");
+  }
+sc.close();
+}
+}
+  
