35fa6216c3538263dfbe8e5d82019c440329856e
gustavoliveira <gustavocoliveira16@gmail.com>
2026-03-12 11:28:08 -0300

    Update ParOuImpar.java

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -1,1 +1,19 @@
 Import java.Util.Scanner
+Public class ParOuImpar {
+  Public Static Void Main(Strings[]args) {
+    Scanner sc = New Scanner(system.in);
+
+    Int numero;
+
+    System.Out.Print("Digite o numero: ");
+    numero = sc.scnextInt();
+
+    if(numero % 2 == 0) {
+      System.Out.Print("O numero é par ");
+    }
+    else{
+      System.Out.Print("O numero é impar");
+    }
+    sc.close();
+  }
+}
