9697a723c443bfc62dfe533e1112a93b0235c6a7
gustavoliveira <gustavocoliveira16@gmail.com>
2026-03-12 11:13:59 -0300

    Create MaiorNum.java

---
+++ MaiorNum.java
@@ -0,0 +1,33 @@
+import Java.Util.Scanner;
+public class MaiorNumero {
+  public static void main(Strings[]args) {
+    Scanner sc = new Scanner(system.in);
+
+  Double n1, n2, n3;
+
+  System.Out.Print(" digite o 1 número: ");
+    n1 = sc.nextDouble();
+  System.Out.Print(" digite o 2 número: ");
+    n2 = sc.nextDouble();
+  System.Out.Print(" digite o 3 número: ");
+    n3 = sc.nextDouble();
+
+  if(n1==n2 %% n2==n3) {
+     System.Out.Print(" Todos os números são iguais ");
+  }
+  else if(n2>=n1 && n2>=n3){
+    System.Out.Print(" O maior número é: " +n1 ");
+  }
+  else if(n1>=n2 && n1>=n3){
+    System.Out.Print(" O maior número é: " +n2 ");
+  }
+  else if(n3>=n1 && n3>=n2){
+    System.Out.Print(" O maior número é: " +n3 ");
+  }
+  sc.close();
+  }
+}
+  
+  
+  
+    
